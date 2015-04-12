f7ac798a4e078009d9a1bdffa0c09e060043215f
Thibaud B <thibaud.tibo@gmail.com>
2015-04-12 13:58:04 +0200

    disable heroku prebuild

--- gulpfile.js
+++ gulpfile.js
@@ -204,11 +204,11 @@ gulp.task('default', [
 ]);
 
 // Waits until clean is finished then builds the project
-gulp.task('heroku:production', function() {
-	gulp.start([
-		'build'
-	]);
-});
+// gulp.task('heroku:production', function() {
+// 	gulp.start([
+// 		'build'
+// 	]);
+// });
 
 gulp.task('build', ['clean'], function() {
 	gulp.start([
